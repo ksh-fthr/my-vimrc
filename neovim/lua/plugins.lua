9b393b070a77ad80d9d45c3c6ee0fe2641bd2438
ksh-fthr <ksh.fthr0806@gmail.com>
2023-02-26 13:32:16 +0900

    Mason でカバーできるので nvim-lsp-installer を削除.

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -73,7 +73,6 @@ packer.startup(function(use)
 
   -- LSP
   use({ "neovim/nvim-lspconfig" }) -- enable LSP
-  use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
   use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
   use({ "glepnir/lspsaga.nvim" }) -- LSP UIs
   use({ 'williamboman/mason.nvim' })
