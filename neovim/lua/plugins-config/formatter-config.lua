fcc0bb85922ecc0c560c8659fc7a41faec93f7a8
ksh-fthr <ksh.fthr0806@gmail.com>
2026-01-30 13:30:39 +0900

    typo修正.

--- neovim/lua/plugins-config/formatter-config.lua
+++ neovim/lua/plugins-config/formatter-config.lua
@@ -29,11 +29,11 @@ null_ls.setup({
     }),
 
     -- Python Formatting
-    null_ls.biultins.formatting.black, -- Python フォーマッター
+    null_ls.builtins.formatting.black, -- Python フォーマッター
     null_ls.builtins.diagnostics.ruff, -- 高速 Python Linter
 
     -- Otherwise
-    null_ls.buitins.diagnostics.fish,   -- fish 診断
+    null_ls.builtins.diagnostics.fish,   -- fish 診断
     null_ls.builtins.formatting.stylua, -- Lua 整形
     null_ls.builtins.formatting.shfmt,  -- Shell 整形
   },
